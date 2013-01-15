ff069f151b42fa74e3472bda1c082c8ddc0ebc73
skrable <vilimets@mlsdev.com>
2013-01-15 16:39:04 +0200

    PartnerApp itunesAppId prefix fixed

--- townWizard-ios/PartnerMenuViewController.m
+++ townWizard-ios/PartnerMenuViewController.m
@@ -163,6 +163,11 @@
 - (void) partnerDetailsLoaded:(Partner *) partner
 {
      [self updateWithPartner:partner];
+    if([partner.iTunesAppId hasPrefix:@"store"])
+    {
+        partner.iTunesAppId = [partner.iTunesAppId stringByReplacingOccurrencesOfString:@"store" withString:@""];
+        
+    }
     [Appirater setAppId:partner.iTunesAppId];
     [AppDelegate sharedDelegate].facebookHelper.appId = partner.facebookAppId;
 
